4c9a96037b57bd0491d42d61d2b8ce93050bf3fb
programador13 <programador13@tecnologicocomfenalco.edu.co>
2024-05-30 16:52:30 -0500

    timeline

--- components/counter-services.tsx
+++ components/counter-services.tsx
@@ -1,17 +1,35 @@
+"use client";
+
+import CountUp from "react-countup";
+
 import { dataCounter } from "@/data";
-import CountUp from "react-countup/build/CountUp";
 
 const CounterServices = () => {
-    return (  
-        <div className="grid justify-between max-w-3xl grid-cols-2 gap-3
-         mx-auto my-8 md:grid-cols.4 md:gap-4">
-            {dataCounter.map(({id,endCounter,text,lineRight,lineRightMobile}) => (
-                <div key={id} className={`${lineRight} && 'ltr'`}>
-                    <p>{text}</p>
-                </div>
-            ))}           
-        </div>
-     );
-}
- 
-export default CounterServices ;
+  return (
+    <div
+      className="grid justify-between max-w-3xl grid-cols-2 gap-3
+         mx-auto my-8 md:grid-cols-4 md:gap-4"
+    >
+      {dataCounter.map(
+        ({ id, endCounter, text, lineRight, lineRightMobile }) => (
+          <div key={id} className={`${lineRight && "ltr"}`}>
+            <div
+              className={`${
+                lineRight &&
+                "px-4 border-2 border-transparent md:border-e-gray-100"
+              } ${lineRightMobile && 'border-e-gray-100'}
+                `}
+            >
+              <p className="flex mb-2 text-2xl font-extrabold md:text-4xl md:text-secondary">
+                + <CountUp end={endCounter} start={0} duration={5} />{" "}
+              </p>
+              <p className="text-xs uppercase max-w-[100px]">{text}</p>
+            </div>
+          </div>
+        )
+      )}
+    </div>
+  );
+};
+
+export default CounterServices;
---
+++ components/timeline.tsx
@@ -0,0 +1,9 @@
+const TimeLine = () => {
+    return (  
+        <div>
+            <h1>TimeLine</h1>
+        </div>
+    );
+}
+ 
+export default TimeLine;
